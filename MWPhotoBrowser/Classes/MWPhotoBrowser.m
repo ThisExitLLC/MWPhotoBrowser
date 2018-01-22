eabaad648a4a214bd5171f95480ab798a542aa5a
fbazbo <fbazbo@gmail.com>
2018-01-21 23:16:32 -0800

    Gotta override UIButton apparently...

--- MWPhotoBrowser/Classes/MWPhotoBrowser.m
+++ MWPhotoBrowser/Classes/MWPhotoBrowser.m
@@ -186,6 +186,7 @@
         _nextButton = [[UIBarButtonItem alloc] initWithImage:[[UIImage imageNamed:[NSString stringWithFormat:arrowPathFormat, @"Right"]] imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate] style:UIBarButtonItemStylePlain target:self action:@selector(gotoNextPage)];
         [_nextButton setTintColor:[UIColor whiteColor]];
     }
+    [UIButton appearance].tintColor = UIColor.whiteColor;
     if (self.displayActionButton) {
         _actionButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAction target:self action:@selector(actionButtonPressed:)];
     }
